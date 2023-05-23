226773d6fcf61c0662760c987683ab994174d88f
Nijumdip <nijumdip019911@gmail.com>
2023-05-23 10:14:04 +0600

    alhamdulillah done count down

--- countdown.js
+++ countdown.js
@@ -1,25 +1,30 @@
-const countDownDate = new Date("oct 28, 2023 00:00:00").getTime();
+const Target = new Date("oct 28, 2023 00:00:00").getTime();
+
+const day = document.getElementById("days");
+const hour = document.getElementById("hours");
+const minute = document.getElementById("minutes");
+const second = document.getElementById("seconds");
 
 const showTime = setInterval(function () {
     const nowDate = new Date().getTime();
-    const distance = countDownDate - nowDate;
+    const distance = Target - nowDate;
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const second = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("second").innerHTML = second;
+    day.innerHTML = days;
+    hour.innerHTML = hours;
+    minute.innerHTML = minutes;
+    second.innerHTML = second;
 
     if (distance < 0) {
         clearInterval(showTime);
 
-        document.getElementById("days").innerHTML = "00";
-        document.getElementById("hours").innerHTML = "00";
-        document.getElementById("minutes").innerHTML = "00";
-        document.getElementById("second").innerHTML = "00";
+        day.innerHTML = "00";
+        hour.innerHTML = "00";
+        minute.innerHTML = "00";
+        second.innerHTML = "00";
     }
 }, 1000);
