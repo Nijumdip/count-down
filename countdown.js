92641be78bd084db65e3bbcbe05c70778da6a2e7
Nijumdip <nijumdip019911@gmail.com>
2023-05-23 12:15:59 +0600

    alhamdulillah done

--- countdown.js
+++ countdown.js
@@ -7,25 +7,30 @@ const second = document.getElementById("seconds");
 
 const showTime = setInterval(function () {
     const currentDate = new Date().getTime();
-    // const distance = Target - currentDate;
-    const distance = (Target - currentDate) / 1000;
+    const dateLine = (Target - currentDate) / 1000;
 
-    const days = Math.floor(distance / 3600 / 24);
-    const hours = Math.floor((distance / 3600) % 24);
-    const minutes = Math.floor((distance / 60) % 60);
-    const seconds = Math.floor(distance % 60);
+    const days = Math.floor(dateLine / 3600 / 24);
+    const hours = Math.floor((dateLine / 3600) % 24);
+    const minutes = Math.floor((dateLine / 60) % 60);
+    const seconds = Math.floor(dateLine % 60);
 
-    // const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    // const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    // const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    // const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // const days = dateLine / 1000 / 3600 / 24;
+    // const hours = dateLine / 1000 / 3600 % 24;
+    // const minutes = dateLine / 1000 / 60 % 60;
+    // const seconds = dateLine / 1000 % 60;
+
+    // const dateLine = Target - currentDate;
+    // const days = Math.floor(dateLine / (1000 * 60 * 60 * 24));
+    // const hours = Math.floor((dateLine % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // const minutes = Math.floor((dateLine % (1000 * 60 * 60)) / (1000 * 60));
+    // const seconds = Math.floor((dateLine % (1000 * 60)) / 1000);
 
     day.innerHTML = days;
     hour.innerHTML = hours;
     minute.innerHTML = minutes;
     second.innerHTML = seconds;
 
-    if (distance < 0) {
+    if (dateLine < 0) {
         clearInterval(showTime);
 
         day.innerHTML = "00";
